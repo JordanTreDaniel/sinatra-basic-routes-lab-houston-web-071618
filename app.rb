b4daeccd1c11096787cb8677feb6d33902c6d37f
Jordan Christley <jordan.christley@flatironschool.com>
2018-08-08 14:21:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,14 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
+  get '/name' do
+    "My name is Jordan"
+  end
+  get '/hometown' do
+    "My hometown is Houston"
+  end
+  get '/favorite song' do
+    "My favorite song is Marvin's Room"
+  end
+  
 end
